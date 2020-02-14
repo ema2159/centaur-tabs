96b7c90bdcb6fe3d7c5146b579ab7b9794c2a17b
ema2159 <ema2159@gmail.com>
2020-02-14 10:51:23 -0600

    Fix: Unnecessary constant buffer group recalculation

--- centaur-tabs.el
+++ centaur-tabs.el
@@ -731,7 +731,7 @@ BEGIN, END and LENGTH are just standard arguments for after-changes-function
 hooked functions"
   (when (not centaur-tabs--idle)
     (setq centaur-tabs--idle t)
-    (run-with-idle-timer 0.5 nil (lambda()
+    (run-with-idle-timer 0.3 nil (lambda()
                                  (setq centaur-tabs--idle nil)
                                  (set-buffer-modified-p (buffer-modified-p))
                                  (centaur-tabs-set-template centaur-tabs-current-tabset nil)
@@ -1855,8 +1855,7 @@ Run as `centaur-tabs-init-hook'."
   (add-hook 'after-save-hook #'centaur-tabs-on-saving-buffer)
   (add-hook 'first-change-hook #'centaur-tabs-on-modifying-buffer)
   (add-hook 'after-change-functions #'centaur-tabs-after-modifying-buffer)
-  (add-hook 'kill-buffer-hook #'centaur-tabs-buffer-track-killed)
-  (add-hook 'buffer-list-update-hook #'centaur-tabs-buffer-update-groups))
+  (add-hook 'kill-buffer-hook #'centaur-tabs-buffer-track-killed))
 
 (defun centaur-tabs-buffer-quit ()
   "Quit tab bar buffer.
@@ -1869,8 +1868,7 @@ Run as `centaur-tabs-quit-hook'."
   (remove-hook 'after-save-hook 'centaur-tabs-after-modifying-buffer)
   (remove-hook 'first-change-hook 'centaur-tabs-on-modifying-buffer)
   (remove-hook 'after-change-functions 'centaur-tabs-after-modifying-buffer)
-  (remove-hook 'kill-buffer-hook 'centaur-tabs-buffer-track-killed)
-  (remove-hook 'buffer-list-update-hook 'centaur-tabs-buffer-update-groups))
+  (remove-hook 'kill-buffer-hook 'centaur-tabs-buffer-track-killed))
 
 (add-hook 'centaur-tabs-init-hook #'centaur-tabs-buffer-init)
 (add-hook 'centaur-tabs-quit-hook #'centaur-tabs-buffer-quit)
