3f29c875eeafad13c0bd12b987d30cf5d5757360
Álvaro González Sotillo <alvarogonzalezsotillo@gmail.com>
2020-08-17 17:51:36 +0200

    There are some redisplay problems. This commit is a test with run-with-idle-timer, to be tested.
     https://github.com/ema2159/centaur-tabs/pull/116#discussion_r471048885

--- centaur-tabs-interactive.el
+++ centaur-tabs-interactive.el
@@ -323,13 +323,6 @@ Should be buffer local and speed up calculation of buffer groups.")
      :action #'centaur-tabs-switch-group
      :caller 'centaur-tabs-counsel-switch-group)))
 
-(defun centaur-tabs--kill-this-buffer-dont-ask()
-  "Kill the current buffer without confirmation."
-  (interactive)
-  (kill-buffer (current-buffer))
-  (centaur-tabs-display-update)
-  (redisplay t))
-
 
 (defun centaur-tabs-extract-window-to-new-frame()
   "Kill the current window in the current frame, and open the current buffer in a new frame."
@@ -405,10 +398,30 @@ Modified copy of `treemacs-visit-node-in-external-application`."
 (defvar centaur-tabs--groups-submenu-key "Tab groups")
 (defvar centaur-tabs--tabs-submenu-key "Go to tab of group")
 
+(defun centaur-tabs--kill-other-buffers-in-current-group-in-a-while()
+  "Intermediate function that try to fix a redisplay problem with `centaur-tabs-kill-other-buffers-in-current-group` when is called from context menu."
+  (interactive)
+  ;;;(centaur-tabs-kill-other-buffers-in-current-group)
+  ;;;(run-at-time 0.1 nil #'centaur-tabs-kill-other-buffers-in-current-group)
+  (run-with-idle-timer 0.1 nil (lambda ()
+				 (centaur-tabs-kill-other-buffers-in-current-group)
+				 (centaur-tabs-display-update)
+				 (redisplay t))))
+
+
+(defun centaur-tabs--kill-this-buffer-dont-ask()
+  "Kill the current buffer without confirmation."
+  (interactive)
+  (run-with-idle-timer 0.1 nil (lambda ()
+				 (kill-buffer (current-buffer))
+				 (centaur-tabs-display-update)
+				 (redisplay t))))
+
+
 (defun centaur-tabs--tab-menu-definition ()
   "Definition of the context menu of a tab."
   `(["Kill this buffer"  centaur-tabs--kill-this-buffer-dont-ask]
-    ["Kill other buffers of group" centaur-tabs-kill-other-buffers-in-current-group]
+    ["Kill other buffers of group" centaur-tabs--kill-other-buffers-in-current-group-in-a-while]
     ["Kill unmodified buffers of group" centaur-tabs-kill-unmodified-buffers-in-current-group]
     "----"
     ["Split below" split-window-below]
